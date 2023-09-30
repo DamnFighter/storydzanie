6390d2f10961f08f96dbe90266b1a7562e12a22d
Leszek Kaliszczuk <lesz3kk@wp.pl>
2023-09-30 17:06:22 +0200

    new xml parser

--- xml_parser.py
+++ xml_parser.py
@@ -1,24 +1,7 @@
-import xml.etree.ElementTree as ET
-
-
-def parse_xml_to_dict(element):
-    result = {}
-    if len(element) == 0:
-        return element.text
-    for child in element:
-        child_data = parse_xml_to_dict(child)
-        if child.tag in result:
-            if type(result[child.tag]) is list:
-                result[child.tag].append(child_data)
-            else:
-                result[child.tag] = [result[child.tag], child_data]
-        else:
-            result[child.tag] = child_data
-    return result
+import mxltodict
 
 
 def get_data(xml_path):
-    tree = ET.parse(xml_path)
-    root = tree.getroot()
-    xml_dict = parse_xml_to_dict(root)
-    return xml_dict
+    with open(SIO_PATH) as f:
+        sio_xml = f.read()
+    return xmltodict.parse(sio_xml)
